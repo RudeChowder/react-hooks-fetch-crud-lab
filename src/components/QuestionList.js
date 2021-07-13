93b900498a1a35bcb3bdf6f6d4227902b8211b60
Stephen Crane <scrane88@gmail.com>
2021-07-13 09:28:07 -0400

    Add GET and POST

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then(resp => resp.json())
+      .then(questions => {
+        setQuestions(questions)
+      })
+  }, [])
+
+  const listOfQuestions = questions.map(question => {
+    return(
+      <QuestionItem 
+        key={question.id}
+        question={question}
+      />
+    )
+  })
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {listOfQuestions}
+      </ul>
     </section>
   );
 }

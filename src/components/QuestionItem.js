e82ba4118c443c699c3e520763aa44911abf9f3d
Kurstyn Storms <kurstyn.storms@powerhrg.com>
2021-07-13 10:24:04 -0400

    core deliverables

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDeleteQuestion, onUpdateQuestion }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,15 +9,23 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  const handleDeleteQuestion = () => {
+    onDeleteQuestion(id)
+  }
+
+  const handleUpdateQuestions =(event) => {
+    onUpdateQuestion(id, parseInt(event.target.value))
+  }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleUpdateQuestions}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDeleteQuestion}>Delete Question</button>
     </li>
   );
 }
